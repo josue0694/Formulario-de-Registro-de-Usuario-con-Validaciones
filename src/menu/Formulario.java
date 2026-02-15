f4b21caa717777cc2b79445400d8d9577e1c80af
Josué <josue0694@hotmail.com>
2026-02-14 23:46:34 -0600

    Primer avance de la tarea

---
+++ src/menu/Formulario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package menu;
+
+/**
+ *
+ * @author Josue
+ */
+public class Formulario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu m= new Menu();
+        m.setVisible(true);//hacemos visible la ventana
+        m.setLocationRelativeTo(null);//coloca el frame en centro de la ventana
+    } 
+}
